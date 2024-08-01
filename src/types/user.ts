326378a1e1adefd9be4d6b3b50b134a100eaac2c
jhkim0704 <108654728+jhkim0704@users.noreply.github.com>
2024-08-01 18:18:45 +0900

    타입 수정

--- src/types/user.ts
+++ src/types/user.ts
@@ -17,8 +17,13 @@ export interface InsertUsersGqlType {
 export interface GetUserQueryVariables {
   id: string;
 }
+// 파라미터에 타입을 직접 명시하지 않는 이유
+// 1. 중복제거 및 유지보수(수정시 해당 타입을 참조하는 모든곳에서 자동 반영)
+// 2. 타입 안정성(해당 타입이 수정되면 참조하는 모든 곳에서 오류가 발생하여 즉시 문제 인식 가능)
+// 3. 명확성(타입명으로 명확한 의미를 나타냄)
+// 4. 일관성 (여러곳에 같은 타입 사용시 실수로 다른 타입을 지정하는것을 방지)
 export interface GetUserGqlType {
-  (id: GetUserQueryVariables): Promise<User>;
+  (id: GetUserQueryVariables['id']): Promise<User>;
 }
 
 export interface GetUserResponse {
